a94fbd75f44e78fbbe25859f8366d4e55d9c300f
jaime canizales <canizales.jaime16@gmail.com>
2024-04-13 11:16:56 -0400

    Update query_selector_example.js

--- class_8_DOM/examples/query_selector_example.js
+++ class_8_DOM/examples/query_selector_example.js
@@ -1,24 +1,24 @@
 //document.query : HTML->body->ul->last(third) li
-// document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "jim";
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "jim";
 
-// let elem=document.querySelector(".list2");
-// elem.innerHTML='JIM';
+let elem=document.querySelector(".list2");
+elem.innerHTML='JIM';
 
-// let elem = document.querySelector("li a");
-// elem.style.color='green';
+elem = document.querySelector("li a");
+elem.style.color='green';
 
-// let elem = document.querySelector("button");
-// elem.style.backgroundColor="yellow";
+elem = document.querySelector("button");
+elem.style.backgroundColor="yellow";
 
-// let elem = document.querySelector("button").classList.add("huge");
-// document.querySelector("button").classList.toggle("huge");
-// document.querySelector("button").classList.toggle("huge");
+elem = document.querySelector("button").classList.add("huge");
+document.querySelector("button").classList.toggle("huge");
+document.querySelector("button").classList.toggle("huge");
 
-//let elem= document.querySelector("").textContent;
+elem= document.querySelector("").textContent;
 
-// let elem = document.querySelector("a").attributes;
-// elem = document.querySelector("a").getAttribute("href");
-// elem = document.querySelector("a").setAttribute("href", "https://www.bingo.com");
+elem = document.querySelector("a").attributes;
+elem = document.querySelector("a").getAttribute("href");
+elem = document.querySelector("a").setAttribute("href", "https://www.bingo.com");
 
 let elems = document.querySelectorAll("li");
 alert(elems);
